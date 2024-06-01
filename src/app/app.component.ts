c5c54eae3e62ebc2da9ae3d80bb69064dfc4ff75
MArcoOverFlow <marco.amador.ma.88@gmail.com>
2024-05-31 23:06:23 -0600

    Primera carga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pantalla2Component } from "./components/pantalla2/pantalla2.component";
+import { Pantalla3Component } from "./components/pantalla3/pantalla3.component";
+import { Pantalla1Component } from "./components/pantalla1/pantalla1.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, Pantalla2Component, Pantalla3Component, Pantalla1Component]
 })
 export class AppComponent {
   title = 'act1DPD';
